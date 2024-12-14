adde3b0aabc280bf79ae94f208b4a9f6e619e32a
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2024-12-14 16:58:49 -0300

    change maint.ts

--- src/main.ts
+++ src/main.ts
@@ -8,25 +8,19 @@ async function bootstrap() {
 
   // Permitir CORS apenas de origens específicas
   app.enableCors({
-    origin: [
-      'http://localhost:3000', // Durante o desenvolvimento no local
-      'https://ocr-project-v2gi.onrender.com', // URL do seu aplicativo em produção no Render
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+    origin: ['http://localhost:3000', 'https://ocr-project-v2gi.onrender.com'], // URL específica
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Permitir envio de cookies, caso necessário
-    preflightContinue: true, // Garantir que as requisições OPTIONS sejam tratadas corretamente
   });
 
-  // Adicionar tratamento para OPTIONS (não é estritamente necessário, pois o NestJS já lida com isso com o `enableCors`)
+  // Adicionar tratamento para OPTIONS
   app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', '*'); // Permitir a origem para todas as requisições
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos HTTP permitidos
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeçalhos permitidos
-      return res.status(204).send(); // Retorna uma resposta vazia com status 204 para requisições OPTIONS
+      res.status(200).send();
+    } else {
+      next();
     }
-    next();
   });
 
   const port = process.env.PORT || 3000;
