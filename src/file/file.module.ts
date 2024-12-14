511d583f0b7a2201e40c26725df2c5138103ee0a
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2024-12-14 15:13:35 -0300

    update fi;e.module.ts

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [FileController],
   providers: [FileService],
 })
