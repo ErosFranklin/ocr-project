5a59943a00a4271aba2c36a0dfeba67e1dafda57
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2024-12-14 18:19:42 -0300

    Update file.module.ts

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { OpenAIModule } from '../openai/openai.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, OpenAIModule],
   controllers: [FileController],
   providers: [FileService],
 })
