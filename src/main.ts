1ffbe08a9313c4ebf1487e654b956b6cae8bf72a
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2024-12-14 16:24:58 -0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,17 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  // Permitir CORS apenas de origens específicas
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', // Durante o desenvolvimento no local
+      'https://ocr-project-v2gi.onrender.com', // URL do seu aplicativo em produção no Render
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
