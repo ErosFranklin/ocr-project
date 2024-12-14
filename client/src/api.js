5e5b6afdb5b44d47ea9f1c3978ee48c27a5dba6d
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2024-12-14 16:48:25 -0300

    update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ocr-project-v2gi.onrender.com', // URL do seu back-end NestJS
+  baseURL: 'https://ocr-project-v2gi.onrender.com',
+  withCredentials: true, 
 });
 
 export const createUser = (data) => api.post('/users/create', data);
