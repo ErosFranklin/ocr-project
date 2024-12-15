4b2afb9631dc668adcc7426fdd9ee20298682ed4
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2024-12-15 11:22:59 -0300

    Updates in css

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createUser, getAllUsers } from './api';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import './style/App.css';
 
 
 function App() {
@@ -8,59 +11,100 @@ function App() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage]  = useState('');
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    getAllUsers().then(response => {
-      if (Array.isArray(response.data)) {
-        setUsers(response.data);
-      } else {
-        console.error('API response is not an array:', response.data);
-      }
-    }).catch(error => {
-      console.error('Error fetching users:', error);
-    });
+    setLoading(true);
+    getAllUsers()
+      .then((response) => {
+        if (Array.isArray(response.data)) {
+          setUsers(response.data);
+        } else {
+          console.error('API response is not an array:', response.data);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching users:', error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
+
 
   const handleCreateUser = async () => {
     try {
       if (!name || !email || !password) {
-        return alert('Preencha todos os campos');
+        return setErrorMessage('Preencha todos os campos');
       }
       if (password.length < 8) {
-        return alert('Senha deve ter no mínimo 8 caracteres');
+        return setErrorMessage('Senha deve ter no mínimo 8 caracteres');
       }
       if (email.indexOf('@') === -1) {
-        return alert('Email inválido');
+        return setErrorMessage('Email inválido');
       }
-
+      setLoading(true)
       const response = await createUser({ name, email, password });
 
       if (!response.data) {
-        return alert('Erro ao criar usuário');
+        return setErrorMessage('Erro ao tentar se cadastrar. Verifique suas credenciais.');
       }
       setUsers(prevUsers => [...prevUsers, response.data]);
       navigate('/login')
     } catch (error) {
       console.error('Erro ao criar usuário:', error);
+      return setErrorMessage('Algo deu errado. Tente novamente.');
+      
+    } finally{
+      setLoading(false)
     }
   };
 
   return (
-    <div className="Create">
-      <h1>NotaOne</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>{user.name} - {user.email}</li>
-        ))}
-      </ul>
-      <div>
-        <h2 className="title-create">Cadastro</h2>
-        <input type="text" placeholder="Nome Completo" value={name} onChange={(e) => setName(e.target.value)} required/>
-        <input type="email" placeholder="exemplo@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} required/>
-        <input type="password" placeholder="********" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-        <button onClick={handleCreateUser}>Cadastrar-se</button>
-      </div>
+    <div className='body'>
+      {loading && (
+        <div className="overlay">
+          <div className="spinner"></div>
+        </div>
+      )}
+        <div className="Create">
+          <div className="title">
+            <h1>NotaOne</h1>   
+          </div>
+          <ul>
+            {users.map(user => (
+              <li key={user.id}>{user.name} - {user.email}</li>
+            ))}
+          </ul>
+          
+          <div className="form-create">
+            <div className="title-container">
+              <h2 className="title-create">Cadastro</h2>
+              <p className="subtitle-create">Olá seja bem-vindo, crie uma conta e comece agora.</p>
+            </div>
+            <div className="inputs-container">
+              <div className="component">
+                <input type="text" placeholder="Nome Completo" value={name} onChange={(e) => setName(e.target.value)} required/>
+                <FontAwesomeIcon className='icone' icon={faUser}/>
+              </div>
+              <div className="component">
+                <input type="email" placeholder="exemplo@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                <FontAwesomeIcon className='icone2' icon={faEnvelope} />
+              </div>
+              <div className="component">
+                <input type="password" placeholder="********" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                <FontAwesomeIcon className='icone3' icon={faLock} />
+              </div> 
+              <span className='message-error'>{errorMessage}</span>
+              <button className='btn-create' onClick={handleCreateUser}>Cadastrar-se</button>
+              
+            </div>
+          </div>
+          <span className='container-botton'>Já possui uma conta? <a href="/login">Faça login</a></span>
+          
+        </div>
     </div>
   );
 }
--- client/src/login.js
+++ client/src/login.js
@@ -1,37 +1,79 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 import { loginUser } from './api';
+import './style/login.css';
 
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage]  = useState('');
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+
 
     const handleLoginUser = async () => {
         try {
             if (!email || !password) {
-                return alert('Preencha todos os campos');
+                return setErrorMessage('Preencha todos os campos');
+            }
+            if(email.indexOf('@') === -1) {
+                return setErrorMessage('Email inválido');
             }
             if (password.length < 8) {
-                return alert('Senha deve ter no mínimo 8 caracteres');
+                return setErrorMessage('Senha deve ter no mínimo 8 caracteres');
             }
+            setLoading(true)
             const response = await loginUser({ email, password });
             if (!response.data) {
-                return alert('Erro ao fazer login');
+                return setErrorMessage('Erro ao fazer login. Verifique suas credenciais.');
             }
             navigate('/menu')
         } catch (error) {
             console.error('Erro ao fazer login:', error);
+            return setErrorMessage('Algo deu errado. Tente novamente.');
+        }finally{
+            setLoading(false)
         }
     };
 
     return (
-        <div className="Login">
-            <h2 className="title-login">Login</h2>
-            <input type="email" placeholder="exemplo@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input type="password" placeholder='********' value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleLoginUser}>Entrar</button>
-        </div>
+        <div className='body'>
+              {loading && (
+                <div className="overlay">
+                  <div className="spinner"></div>
+                </div>
+              )}
+                <div className="Login">
+                  <div className="title">
+                    <h1>NotaOne</h1>   
+                  </div>
+                  
+                  <div className="form-login">
+                    <div className="title-container">
+                      <h2 className="title-login">Login</h2>
+                      <p className="subtitle-login">Olá novamente, realize o login e continue usando.</p>
+                    </div>
+                    <div className="inputs-container">
+                      <div className="component">
+                      <input type="email" placeholder="exemplo@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} required aria-label="Digite seu email"/>
+                        <FontAwesomeIcon className='icone2' icon={faEnvelope} />
+                      </div>
+                      <div className="component">
+                      <input type="password" placeholder='********' value={password} onChange={(e) => setPassword(e.target.value)} required aria-label="Digite sua senha"/>
+                        <FontAwesomeIcon className='icone3' icon={faLock} />
+                      </div> 
+                      <span className='message-error'>{errorMessage}</span>
+                      <button className='btn-login' onClick={handleLoginUser}>Entrar</button>
+                      
+                    </div>
+                  </div>
+                  <span className='container-botton'>Nao possui uma conta? <a href="/">Registre-se agora</a></span>
+                  
+                </div>
+            </div>
+
     )
 }
 
